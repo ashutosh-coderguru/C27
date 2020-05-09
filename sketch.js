d3b92900023a7e1c6a1a7507c8aa8fa9178b3710
ashutosh-coderguru <64152627+ashutosh-coderguru@users.noreply.github.com>
2020-05-09 19:50:41 +0530

    Constraint bro

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint=Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
@@ -23,6 +24,7 @@ function setup(){
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
     log1 = new Log(810,260,300, PI/2);
+  
 
     box3 = new Box(700,240,70,70);
     box4 = new Box(920,240,70,70);
@@ -33,9 +35,10 @@ function setup(){
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
+    log6 = new Log(200,120,150, -PI/7);
 
     bird = new Bird(100,100);
-
+   chain=new Chain(bird.body,log6.body);
 }
 
 function draw(){
@@ -54,11 +57,11 @@ function draw(){
     box4.display();
     pig3.display();
     log3.display();
-
+    log6.display();    
     box5.display();
     log4.display();
     log5.display();
-
+    chain.display();
     bird.display();
     platform.display();
 }
